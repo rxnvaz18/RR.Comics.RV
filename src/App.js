e3023a21348f0dbc61bde4cb3953f10df205076a
unknown <roxanvaz31@gmail.com>
2024-01-06 17:55:23 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+
+import UserInterface from "./UserInterface"
+
+function App() {
+  const stylin = {
+    background: '#1ECD97',
+    display: 'inline-block',
+    width: 120,
+    height: 55,
+    fontSize: 18,
+    letterSpacing: 1,
+    border: '2x solid #1ECD97',
+    borderRadius: 15
+  }
+
+  return (
+    <main>
+     <h1
+          style={{
+            color: "#FEE001",
+            fontFamily: "Luminari, fantasy",
+            fontSize: 50
+          }}
+        >
+          Welcome to Comics Galore!
+        </h1>
+        <UserInterface />
+    </main>
+  );
+}
+
+export default App;
